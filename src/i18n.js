7309d70c372b378e2a4c601ec9a4ad0482404881
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-17 08:03:57 -0500

    chore: add vue-i18n and configure it

---
+++ src/i18n.js
@@ -0,0 +1,13 @@
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+    legacy: false,
+    locale: "en",
+    fallbackLocale: "en",
+    messages: {en, es}
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18n from "@/i18n.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(i18n)
+    .mount('#app')
