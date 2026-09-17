f6fc8497c62856d9db2758524cdc886be73c9afc
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-17 08:42:23 -0500

    chore: add primevue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from "@/i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Avatar, Button, SelectButton} from "primevue";
+
+const primeUiLicenseKey = "eyJpZCI6ImFmM2U5YjFlLTM4NzEtNGU2MS1iNDk1LTViMTA5YzY2OTBhNiIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODk1Njk3NjUsImV4cCI6MTgyMTEwNTc2NX0.pRhfdy6wjsRrLJsQxIbwhpJHcgCsPc4uvUJgHrioOC7TaXn0ZM_6aTi-9d3a9toGCy-yzbO12aO4in6xkJyZBQ";
 
 createApp(App)
     .use(i18n)
+    .use(PrimeVue, {
+        ripple: true,
+        theme: { preset: Material },
+        license: primeUiLicenseKey
+    })
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
     .mount('#app')
