fdab61b08423eacba582a7bb07473aff8f4ccc38
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-22 09:22:52 -0500

    feat(shared): fix news-api consts

--- src/news/infrastructure/news-api.js
+++ src/news/infrastructure/news-api.js
@@ -4,7 +4,7 @@ import {errorInterceptor} from "@/shared/infrastructure/error.interceptor.js";
 const newsApi = import.meta.env.VITE_NEWS_API_URL;
 const apiKey = import.meta.env.VITE_NEWS_API_KEY;
 const sourcesEndpoint = import.meta.env.VITE_SOURCES_ENDPOINT_PATH;
-const topHeadlinesEndpoint = import.meta.end.VITE_TOP_HEADLINES_ENDPOINT_PATH;
+const topHeadlinesEndpoint = import.meta.env.VITE_TOP_HEADLINES_ENDPOINT_PATH;
 
 const http = axios.create({
     baseURL: newsApi,
