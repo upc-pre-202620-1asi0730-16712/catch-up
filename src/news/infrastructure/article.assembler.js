99f6b01c66a41b8693b8239487e777971ae5eeae
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-22 08:30:58 -0500

    feat(news): add article assembler

---
+++ src/news/infrastructure/article.assembler.js
@@ -0,0 +1,45 @@
+import {SourceAssembler} from "@/news/infrastructure/source.assembler.js";
+import {Article} from "@/news/domain/model/article.entity.js";
+
+export class AssetApi {
+    #source;
+    #sourceAssembler;
+
+    constructor(source = null) {
+        this.#source = source;
+        this.#sourceAssembler = new SourceAssembler();
+    }
+
+    toEntitiesFromResponse(response) {
+        if (response.data.status !== "ok") {
+            console.error(response.data.message);
+            return [];
+        }
+        const articlesResponse = response.data;
+        return articlesResponse["articles"].map((article) => {
+            try {
+                return this.toEntityFromResource(article);
+            } catch (error) {
+                console.error('Validation error for article:', error.message, article);
+                return null;
+            }
+        }).filter(article => article !== null);
+    }
+
+    toEntityFromResource(resource) {
+        let article = new Article({
+            ...resource,
+            source: resource.source || {name: 'Unknown Source'}
+        });
+        article.source = this.#source && (
+            this.#source.id === resource.source?.id || this.#source.name === resource.source?.name
+        )
+            ? this.#source
+            : this.#sourceAssembler.toEntityFromResource(
+                resource.source || {id: 'unknown', name: 'Unknown Source'},
+            );
+
+        return article;
+    }
+
+}
