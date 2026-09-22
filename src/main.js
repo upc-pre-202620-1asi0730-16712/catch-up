dee127c6df4d14c7f82605f8267577cef11018bf
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-22 07:31:59 -0500

    feat(main): add stylesheet and config primevue components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
+import './style.css';
 import App from './App.vue'
 import i18n from "@/i18n.js";
 import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import {Avatar, Button, SelectButton} from "primevue";
+import {Avatar, Button, Card, Drawer, Menu, Menubar, Popover, SelectButton, Toolbar, Tooltip} from "primevue";
 
 const primeUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 
@@ -19,4 +20,11 @@ createApp(App)
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
     .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-card', Card)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-popover', Popover)
+    .directive('tooltip', Tooltip)
     .mount('#app')
